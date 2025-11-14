d6734053495116edacb1e1b60d4edce3c8a3e281
axwaydemouseruk@gmail.com <axwaydemouseruk@gmail.com>
2025-11-14 09:17:11 +0000

    Version 501

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjI3MTA4MQ/spectral.js?branch_slug=main&token=789697cb-7e31-463c-9dbb-084fa1bd9c00";
+export default { extends: ruleset };
